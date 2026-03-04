4ee3fa97f0df16e6ff984deefcdde7ed9289e02f
Hall <oncemeet0857@gmail.com>
2026-03-04 11:30:23 +0800

    feat: update linkedin

--- src/store/app.ts
+++ src/store/app.ts
@@ -2,8 +2,7 @@ export const useAppStore = defineStore("app", () => {
   const state = {
     test: "Hello, World!"
   };
-  const linkedin =
-    "https://www.linkedin.com/in/%E8%81%96%E5%82%91-%E6%9D%8E-a242b2388/";
+  const linkedin = "https://www.linkedin.com/in/hal-lee-a242b2388/";
   const mail = "oncemeet0857@gmail.com";
 
   return {
